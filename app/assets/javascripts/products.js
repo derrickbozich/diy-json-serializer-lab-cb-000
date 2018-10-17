59eac205dd62d96dba0f1ef96d2f15cfcc944e6c
derrick b <dbozich1@gmail.com>
2018-10-17 04:01:14 +0000

    automatically backed up by learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -5,12 +5,25 @@ $(function(){
     const nextId = parseInt(this.dataset.id) + 1;
     const url = `products/${nextId}/data`
     debugger
-    $.getJSON('products/2/data', function(json){
+    $.getJSON(url, function(json){
       debugger
     })
-
-
   })
+
+
+  $(".js-more").on("click", function() {
+    const id = $(this).data("id");
+    $.get("/products/" + id + "/description", function(description) {
+      $.get("/products/" + id + "/inventory", function(inventory) {
+        var inventoryText = "<strong>Available</strong>";
+        if(inventory === "false"){
+          inventoryText = "<strong>Sold Out</strong>";
+        }
+        var descriptionText = "<p>" + description + "</p><p>" + inventoryText + "</p>";
+        $("#product-" + id).html(descriptionText);
+      });
+    });
+  });
 
 
 })
