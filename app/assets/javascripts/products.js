333311e411568414f495c285d8a34469fdd54b0c
derrick b <dbozich1@gmail.com>
2018-10-17 03:49:45 +0000

    automatically backed up by learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,7 +1,7 @@
 $(function(){
   $('#next-product-link').on('click', function(e){
     console.log("hijacked the link!");
-    const nextId = parseInt(this.dataset.id) + 1
+    const nextId = parseInt(this.dataset.id) + 1;
     const url = `products/${nextId}/data`
     debugger
     $.getJSON(url, function(json){
