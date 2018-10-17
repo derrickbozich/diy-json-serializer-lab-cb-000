704203204064018f7541ab12ce53d81cf8d62954
derrick b <dbozich1@gmail.com>
2018-10-17 04:03:58 +0000

    automatically backed up by learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -11,19 +11,19 @@ $(function(){
   })
 
 
-  // $(".js-more").on("click", function() {
-  //   const id = $(this).data("id");
-  //   $.get("/products/" + id + "/description", function(description) {
-  //     $.get("/products/" + id + "/inventory", function(inventory) {
-  //       var inventoryText = "<strong>Available</strong>";
-  //       if(inventory === "false"){
-  //         inventoryText = "<strong>Sold Out</strong>";
-  //       }
-  //       var descriptionText = "<p>" + description + "</p><p>" + inventoryText + "</p>";
-  //       $("#product-" + id).html(descriptionText);
-  //     });
-  //   });
-  // });
+  $(".js-more").on("click", function() {
+    const id = $(this).data("id");
+    $.get("/products/" + id + "/description", function(description) {
+      $.get("/products/" + id + "/inventory", function(inventory) {
+        var inventoryText = "<strong>Available</strong>";
+        if(inventory === "false"){
+          inventoryText = "<strong>Sold Out</strong>";
+        }
+        var descriptionText = "<p>" + description + "</p><p>" + inventoryText + "</p>";
+        $("#product-" + id).html(descriptionText);
+      });
+    });
+  });
 
 
 })
