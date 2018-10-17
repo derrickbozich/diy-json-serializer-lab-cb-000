c6ae65affc314f8114f6e86bd5cf5b19f434bc26
derrick b <dbozich1@gmail.com>
2018-10-17 04:10:55 +0000

    automatically backed up by learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,28 +1,12 @@
 $(function(){
   $('.js-next').on('click', function(e){
     e.preventDefault();
-    console.log("hijacked the link!");
     const nextId = parseInt(this.dataset.id) + 1;
     const url = `/products/${nextId}/data`
-    debugger
     $.getJSON(url, function(json){
       debugger
     })
   })
-
-
-  // $(".js-next").on("click", function() {
-  //   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-  //   $.get("/products/" + nextId + "/data", function(data) {
-  //     $(".productName").text(data["name"]);
-  //     $(".productPrice").text(data["price"]);
-  //     $(".productDescription").text(data["description"]);
-  //     $(".productInventory").text(data["inventory"]);
-  //     // re-set the id to current on the link
-  //     $(".js-next").attr("data-id", data["id"]);
-  //   });
-  // });
-
 
 
   $(".js-more").on("click", function() {
